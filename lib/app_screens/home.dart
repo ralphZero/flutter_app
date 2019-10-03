65c886365afae1deb13144c02eca6e21fd246d75
ralph <placideralph@gmail.com>
2019-10-03 02:23:34 -0400

    Exploring widgets

---
+++ lib/app_screens/home.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        alignment: Alignment.center,
+        color: Colors.lightGreen,
+        //width: 200.0,
+        //height: 100.0,
+        //margin: EdgeInsets.all(10.0),
+        child: Text(
+          "Hello part. 2",
+          textDirection: TextDirection.ltr,
+          style: TextStyle(
+              color: Colors.white,
+              fontSize: 30.0
+          ),
+        ),
+      ),
+    );
+
+
+  }
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app/app_screens/first_screen.dart';
 
-void main() => runApp(MyFlutterApp());
+import 'app_screens/home.dart';
 
-class MyFlutterApp extends StatelessWidget{
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: "My Flutter App",
-        home:
-        Scaffold(
-          appBar: AppBar(
-            title: Text("My Flutter app"),
-          ),
-          body: FirstScreen()
-        )
-    );
-  }
-
+void main(){
+  runApp(
+    MaterialApp(
+      title: "Exploring Flutter widgets",
+      home: Home(),
+    )
+  );
 }
