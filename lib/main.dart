11f6d2d8070300252ec7cafad0375ed6e9135660
ralph <placideralph@gmail.com>
2019-10-03 01:22:23 -0400

    AppBar and scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,29 @@ import 'package:flutter/material.dart';
 void main(){
 
   runApp(
-    Center(
-      child: Text(
-        "Hello world",
-        textDirection: TextDirection.ltr,
-      ),
+
+    MaterialApp(
+      title: "My Flutter App",
+      home:
+      Scaffold(
+        appBar: AppBar(
+          title: Text("My Flutter app"),
+        ),
+        body: Material(
+          color: Colors.deepOrange,
+          child: Center(
+            child: Text(
+                "Hello world",
+                textDirection: TextDirection.ltr,
+                style: TextStyle(color: Colors.white, fontSize: 24.0)
+            ),
+          ),
+        ),
+      )
+
+
     )
+
+
   );
 }
